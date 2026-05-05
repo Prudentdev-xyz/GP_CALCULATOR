32b1124662f73e72fc928955358401b0fc3cedb4
Prudentdev-xyz <your-github-email@gmail.com>
2026-05-05 08:06:02 +0100

    Fix static files with whitenoise

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
-web: gunicorn gp_calculator.wsgi
+
+
--- gp_calculator/settings.py
+++ gp_calculator/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -135,3 +136,6 @@ LOGIN_REDIRECT_URL = '/auth/dashboard/'
 
 import os
 ALLOWED_HOSTS = ['*']
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
